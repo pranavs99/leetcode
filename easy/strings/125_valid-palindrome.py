82c692464018f881ae68a014794451d72c2dbff2
pranavs99 <pranavs99@github.com>
2020-09-12 15:14:21 -0700

    added exercise 125

---
+++ easy/strings/125_valid-palindrome.py
@@ -0,0 +1,7 @@
+def isPalindrome(self, s: str) -> bool:
+    # filter out all characters for whom .isalnum() returned False
+    alnum_only = list(filter(lambda x: x.isalnum(), [c for c in s]))        
+    # lower case all letters
+    lowercased = list(map(lambda x: x.lower() if x.isalpha() else x, alnum_only))  
+    # check if it reads the same forwards and backwards
+    return list(reversed(lowercased)) == lowercased
